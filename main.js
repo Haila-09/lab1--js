344977d743850244ebe9ca4c4d5f38cb1e7ad869
Haila Nasser <hailaalnasser94@gmail.com>
2024-05-22 18:02:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+// Question number1
+for(let i=0; i <= 25 ; i++){
+    console.log(i)
+}
+
+
+// Question number2
+for(let i=0; i <= 10 ; i++){
+    if(i % 2 != 0){
+        console.log(i)
+    }
+  
+}
+
+// Question number3
+for(let i=0; i <= 10 ; i++){ 
+    console.log(i * i)
+}
+
+
+// Question
+for (let i = 1 ; i <= 9 ; i++){
+    for(let j=1 ; j <= 5 ; j++){
+        console.log(i*1)
+        console.log(i*2)
+        console.log(i*3)
+    }
+}
+
+
+let num1 = 0;
+let num2 = 1;
+for (let i = 1; i <= 10; i++) {
+    console.log(num1);
+    result = num1 + num2;
+    num1 = num2;
+    num2 = result;
+}
+
+
+
